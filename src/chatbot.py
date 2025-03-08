290c52da4e7c369c033dce186d71f324efb60c97
Arasp Shojai <Shojaiarasp@outlook.de>
2025-03-08 12:15:39 +0100

    minimale Anpassung des Chatbots

--- src/chatbot.py
+++ src/chatbot.py
@@ -12,13 +12,16 @@ def chatbot_antwort(eingabe):
     eingabe = eingabe.lower()
     for schluesselwort in datenbank:
         if schluesselwort in eingabe and schluesselwort != "fallback":
-            return datenbank[schluesselwort]
+            antwort = datenbank[schluesselwort]
+            if isinstance(antwort, list):
+                return random.choice(antwort)
+            return antwort
     
     return random.choice(datenbank["fallback"])
 
 if __name__ == "__main__":
     while True:
-        frage = input("Frage mich etwas über deutsche Politik und Geschichte: ")
+        frage = input("Frage mich etwas über deutsche Politik und Geschichte oder stelle eine allgemeine Frage: ")
         if frage.lower() in ["exit", "quit", "ende"]:
             print("Bis bald!")
             break
